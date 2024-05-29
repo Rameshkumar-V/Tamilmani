9ff14af9fdf12d1fe991e369912a54fdb322e3d6
RAMESHKUMAR <146409531+Rameshkumar-V@users.noreply.github.com>
2024-05-29 20:16:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,38 @@
+# app.py
 
-# FLASK ATTRIBUTES
-from flask import (
-    Flask,
-    render_template,
-    request,
-    redirect,
-    url_for,
-    jsonify,
-    send_file,
-    flash
-)
-# IMAGE BYTES HANDLER
+from flask import Flask, render_template, request, redirect, url_for, jsonify, send_file, flash
 import io
-
-# DATABASE ORM HANDLER
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-# PAGINATION HANDLER
 from flask_paginate import Pagination, get_page_args
-
-# SECURITY HASH PASSWORD HANDLER
 from werkzeug.security import check_password_hash, generate_password_hash
-
-# DATABASE MODULE HANDLER ( OUR MODULE )
-from database import db, Contact, Document, Category, ContactInfo, PageInformation,DocumentView,ProfileAbout
-
-# AUTHENTICATION AND LOGIN HANDLER
-from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user,login_required
-
-# FLASK ADMIN HANDLER
+from database import db, Contact, Document, Category, ContactInfo, PageInformation, DocumentView, ProfileAbout
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import AdminIndexView, Admin
-# NOTE : FORM OPERATION 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length
+import click
 
 app = Flask(__name__, template_folder='template')
 
 # Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmproject.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] ="mysql://rk:rk@localhost/blog"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://rk:rk@localhost/blog"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'mysecret'
 
 # INITIALIZE DB
 db.init_app(app)
-
 migrate = Migrate(app, db)
 login = LoginManager(app)
 
-
-
-'''
-ADMIN SECTION
-
-'''
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
-# RE CONFIGURE MODELVIEW
 class MyModelView(ModelView):
     def is_accessible(self):
         return current_user.is_authenticated
@@ -76,19 +46,7 @@ class MyAdminIndexView(AdminIndexView):
     def inaccessible_callback(self, name, **kwargs):
         return redirect(url_for('login'))
 
-# with app.app_context():
-#         db.create_all()
-#         print("Db created sucess       ddddddddddddddddddddddddddddddddddddddddddddddddd")
-#         if not User.query.filter_by(username='tm').first():
-#             new_user = User(username='tm', password=generate_password_hash('1234'))
-#             db.session.add(new_user)
-#             db.session.commit()
-#             print("DATABASE CREATED IF")
-# ADMIN INITIALIZING
-db_admin = Admin(app, name='microblog', template_mode='bootstrap3',index_view=MyAdminIndexView())
-
-# NOTE : add_view used to add db_admin pannel inside model based CRUD operations.
-
+db_admin = Admin(app, name='microblog', template_mode='bootstrap3', index_view=MyAdminIndexView())
 db_admin.add_view(DocumentView(Document, db.session))
 db_admin.add_view(ModelView(Category, db.session))
 db_admin.add_view(ModelView(Contact, db.session))
@@ -97,38 +55,21 @@ db_admin.add_view(ModelView(ContactInfo, db.session))
 db_admin.add_view(ModelView(ProfileAbout, db.session))
 db_admin.add_view(MyModelView(User, db.session))
 
-
-
 @login.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
-'''
-END OF ADMIN SECTION
 
-'''
-
-
-# HOME PAGE
 @app.route('/')
 def home():
-    print("Home page vanthuta ddddddddddddddddddddddddddddddddddd")
     page_data = PageInformation.query.first()
     contact_info_data = ContactInfo.query.all()
     categories = Category.query.all()
     return render_template('home.html', categories=categories, contact=contact_info_data, page_info=page_data)
 
-
-
-
-
-
-# THANKYOU FOR CONTACT US
 @app.route('/thank_you')
 def thank_you():
     return "Thank you for your message! We'll get back to you soon."
 
-
-# DOCUMENT GETTING
 def get_documents(page, per_page, category_id=None, search_term=None):
     query = Document.query
     if category_id:
@@ -137,9 +78,6 @@ def get_documents(page, per_page, category_id=None, search_term=None):
         query = query.filter(Document.document_filename.ilike(f'%{search_term}%'))
     return query.paginate(page=page, per_page=per_page, error_out=False)
 
-
-
-# DOWNLOAD PAGE
 @app.route('/download_page')
 def download_page():
     category_id = request.args.get('category_id', type=int)
@@ -148,8 +86,6 @@ def download_page():
     categories = Category.query.all()
     return render_template('download_page.html', documents=documents.items, pagination=documents, categories=categories, current_category=category_id)
 
-
-# SEARCH ROUTE
 @app.route('/search')
 def search_documents():
     search_term = request.args.get('q', '').strip()
@@ -161,8 +97,6 @@ def search_documents():
     pagination = Pagination(page=page, total=documents.total, per_page=per_page, css_framework='bootstrap4')
     return render_template('document_list.html', documents=documents.items, pagination=pagination)
 
-
-# DOCUMENT GETTING WITH ID FOR DOWNLOAD
 @app.route('/get_document', methods=['GET'])
 def get_document():
     document_id = request.args.get('document_id')
@@ -180,7 +114,6 @@ def get_document():
         download_name=document.document_filename
     )
 
-# CONTACT US PAGE TO DB ROUTE
 @app.route('/submit_contact_form', methods=['POST'])
 def submit_contact_form():
     name = request.form.get('name')
@@ -193,30 +126,20 @@ def submit_contact_form():
         return redirect(url_for('thank_you'))
     return redirect(url_for('contact'))
 
-# NOTE : PROFILE ABOUT PAGE
-# -------------------------
 @app.route('/profile')
 def profile():
-
     profiles = ProfileAbout.query.all()
-    
     formatted_profiles = []
-    
 
     for profile in profiles:
         formatted_profile = {
             'title': profile.title,
-            # Replace \n with <br> tags
             'detail': profile.detail.split('/n')
         }
-        print(formatted_profile)
         formatted_profiles.append(formatted_profile)
     
     return render_template('profile.html', profiles=formatted_profiles)
 
-
-# NOTE : LOGIN FORM
-# ------------------
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=25)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=25)])
@@ -232,29 +155,26 @@ def login():
         if user and check_password_hash(user.password, password):
             login_user(user)
             flash('Logged in successfully.', 'success')
-            
             return redirect(url_for('admin.index'))
         else:
             flash('Invalid username or password.', 'danger')
     return render_template('login.html', form=form)
 
-
-
-# LOGOUT ROUTE
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('home')) 
+    return redirect(url_for('home'))
 
+@click.command(name='init-db')
+def init_db():
+    db.create_all()
+    if not User.query.filter_by(username='tm').first():
+        new_user = User(username='tm', password=generate_password_hash('1234'))
+        db.session.add(new_user)
+        db.session.commit()
+        print("Database tables created and initial user added")
 
+app.cli.add_command(init_db)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        if not User.query.filter_by(username='tm').first():
-            new_user = User(username='tm', password=generate_password_hash('1234'))
-            db.session.add(new_user)
-            db.session.commit()
-            print("DATABASE CREATED IF")
     app.run()
-
